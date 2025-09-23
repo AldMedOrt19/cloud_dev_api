dfdc9458baa31559c666d5d6993e7d7dab3df926
Aldana Arleth Medina Ortiz <aldana.medina.dft.team@gmail.com>
2025-09-23 15:23:04 -0500

    Edit main,outputs and variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "comportamiento_public_ip"  { value = aws_instance.comportamiento.public_ip }
-output "comportamiento_test_url"   { value = "http://${aws_instance.comportamiento.public_ip}:8000/comportamiento?city=Lima&date=2025-09-23" }
-output "ssh_hint"                  { value = "ssh -i <KEY>.pem debian@${aws_instance.comportamiento.public_ip} (o admin@)" }
+output "comportamiento_public_ip" { value = aws_instance.comportamiento.public_ip }
+output "comportamiento_test_url" { value = "http://${aws_instance.comportamiento.public_ip}:8000/comportamiento?city=Lima&date=2025-09-23" }
+output "ssh_hint" { value = "ssh -i <KEY>.pem debian@${aws_instance.comportamiento.public_ip} (o admin@)" }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
-variable "aws_region"       { default = "us-east-2" }
-variable "key_name"         { description = "Nombre de la key pair existente" }
-variable "repo_url"         { default = "https://github.com/EliLuxurious/cloud_dev_api.git" }
+variable "aws_region" { default = "us-east-2" }
+variable "key_name" { description = "Nombre de la key pair existente" }
+variable "repo_url" { default = "https://github.com/EliLuxurious/cloud_dev_api.git" }
 variable "elia_public_host" { default = "18.221.212.253" } # IP/DNS de tu EC2 A
-variable "instance_type"    { default = "t3.small" }
-variable "open_to_world"    { default = true }
-variable "my_ip_cidr"       { default = "0.0.0.0/0" }
+variable "instance_type" { default = "t3.small" }
+variable "open_to_world" { default = true }
+variable "my_ip_cidr" { default = "0.0.0.0/0" }
